2f128993a97a40275588ba9356f5ed8763f0b8b7
rosercoder <rosercoder@gmail>
2022-06-03 13:42:59 +0800

    (test) Init Test Class

---
+++ src/test/java/cool/wangshuo/album/AlbumApplicationTests.java
@@ -0,0 +1,19 @@
+package cool.wangshuo.album;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class AlbumApplicationTests {
+
+    @Test
+    public void contextLoads() {
+
+    }
+}

4b176046b8d0d7dbdb27fc51a632ce23b54d4874
rosercoder <rosercoder@gmail>
2022-06-09 21:07:29 +0800

    (test) Update Test Class

--- src/test/java/cool/wangshuo/album/AlbumApplicationTests.java
+++ src/test/java/cool/wangshuo/album/AlbumApplicationTests.java
@@ -12,8 +12,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 public class AlbumApplicationTests {
 
+    @Autowired
+    private StringEncryptor stringEncryptor;
+
     @Test
     public void contextLoads() {
-
+        System.out.println(stringEncryptor.encrypt(""));
     }
+
 }

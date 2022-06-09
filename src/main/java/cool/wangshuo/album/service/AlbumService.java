e2ed3677e8b175e93a7fba82b16a3cb3a1eeb345
rosercoder <rosercoder@gmail>
2022-06-09 20:01:25 +0800

    (Feat) Add AlbumService and AlbumServiceImp Class

---
+++ src/main/java/cool/wangshuo/album/service/AlbumService.java
@@ -0,0 +1,28 @@
+package cool.wangshuo.album.service;
+
+import com.alibaba.fastjson.JSONObject;
+import cool.wangshuo.album.entity.AlbumEntity;
+
+import java.util.List;
+
+/**
+ * @author wangsh
+ * @date 2022/5/3 13:33
+ */
+
+public interface AlbumService {
+
+    public Integer insert(AlbumEntity album);
+
+    public AlbumEntity queryById(Integer albumId);
+
+    public List<JSONObject> queryAllByLimit(AlbumEntity album);
+
+    public List<JSONObject> queryByPage(Integer page, Integer limit);
+
+    public List<AlbumEntity> queryAll();
+
+    public Integer update(AlbumEntity album);
+
+    public Integer delete(Integer albumId);
+}
---
+++ src/main/java/cool/wangshuo/album/service/imp/AlbumServiceImp.java
@@ -0,0 +1,68 @@
+package cool.wangshuo.album.service.imp;
+
+import com.alibaba.fastjson.JSONObject;
+import cool.wangshuo.album.entity.AlbumEntity;
+import cool.wangshuo.album.mapper.AlbumMapper;
+import cool.wangshuo.album.service.AlbumService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @author wangsh
+ * @date 2022/4/30 0:26
+ */
+
+@Service
+public class AlbumServiceImp implements AlbumService {
+
+    @Resource
+    private AlbumMapper albumMapper;
+
+
+
+    @Override
+    public Integer insert(AlbumEntity album) {
+        return albumMapper.insert(album);
+    }
+
+    @Override
+    public AlbumEntity queryById(Integer albumId) {
+       return albumMapper.queryById(albumId);
+    }
+
+    /**
+     * 限制过滤条件、查询符合条件的相册列表
+     * @param album
+     * @return
+     */
+    @Override
+    public List<JSONObject> queryAllByLimit(AlbumEntity album){
+        return albumMapper.queryAllByLimit(album);
+    }
+
+    @Override
+    public List<JSONObject> queryByPage(Integer page, Integer limit) {
+        if (page == null && limit == null) {
+            return albumMapper.queryAllPublic();
+        }
+        Integer fromIndex = (page - 1) * limit; // 起始位置
+        return albumMapper.queryAllPublic();
+    }
+
+    @Override
+    public List<AlbumEntity> queryAll() {
+        return albumMapper.queryAll();
+    }
+
+    @Override
+    public Integer update(AlbumEntity album) {
+        return this.albumMapper.update(album);
+    }
+
+    @Override
+    public Integer delete(Integer albumId) {
+        return this.albumMapper.deleteById(albumId);
+    }
+}

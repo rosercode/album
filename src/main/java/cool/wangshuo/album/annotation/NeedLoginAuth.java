c202053c0c49b87742d2b14c6e299c3dc310311a
rosercoder <rosercoder@gmail>
2022-06-12 23:21:52 +0800

    (Style) Add Comment

--- src/main/java/cool/wangshuo/album/annotation/NeedLoginAuth.java
+++ src/main/java/cool/wangshuo/album/annotation/NeedLoginAuth.java
@@ -3,8 +3,15 @@ package cool.wangshuo.album.annotation;
 import java.lang.annotation.*;
 
 /**
- * 标记需要登录的接口
+ * 标记需要登录的接口 <br>
  * 拥有这个注解的方法接口，需要登录 <br>
+ *
+ *
+ * 权限说明
+ * 1. 不需要登录即可访问
+ * 2. 需要登录，但是不要求是管理员权限
+ * 3. 需要登录，要求是管理员权限（用于管理员权限已经是登录用户的身份了）
+ *
  */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)

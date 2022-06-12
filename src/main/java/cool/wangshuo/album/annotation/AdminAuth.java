fbf4654a0578119caa17abf6c4c5b6f48e336c09
rosercoder <rosercoder@gmail>
2022-06-12 23:06:42 +0800

    (Style) Add Comment

--- src/main/java/cool/wangshuo/album/annotation/AdminAuth.java
+++ src/main/java/cool/wangshuo/album/annotation/AdminAuth.java
@@ -3,7 +3,7 @@ package cool.wangshuo.album.annotation;
 import java.lang.annotation.*;
 
 /**
- * 需要管理员权限
+ * 需要管理员权限 【管理员表明是已经登录的】
  */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)

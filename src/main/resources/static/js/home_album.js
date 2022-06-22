9b32effc80286522fa98d185c868ca8907f33907
rosercoder <rosercoder@gmail>
2022-06-22 17:08:18 +0800

    refactor: refactor front-end code(html)
    - refactor: divide module(home.html) into some modules(including js).
    - style: modify the style in front-end.
    - fix: fix small bugs.

--- src/main/resources/static/js/home_album.js
+++ src/main/resources/static/js/home_album.js
@@ -1,13 +1,12 @@
 var mainData = {
-    albumList:[],
-    pictureList:[]
+    albumList:[]
 }
 new Vue({
     el: "#main",
     data: mainData,
     created: function () {
         this.hello2()
-        this.hello3()
+
     },
     updated: function () {
 
@@ -19,34 +18,14 @@ new Vue({
                 .then(function (response) {
                     // 处理成功情况
                     // console.log(response);
-                    mainData.albumList = response.data
+                    const res = response.data
+                    mainData.albumList = res.data
                     alertify.notify("相册列表获取成功", 'success', 3, function () {
                     });
                 })
                 .catch(function (error) {
                     // 处理错误情况
                     alertify.notify("相册列表获取失败" + error, 'success', 3, function () {
-                    });
-                    // console.log(error);
-                })
-                .then(function () {
-                    // 总是会执行
-                });
-        },
-        // 获取照片列表
-        hello3: function () {
-            axios.get('/picture/queryAll?source=2')
-                .then(function (response) {
-                    // 处理成功情况
-                    // console.log(response);
-                    const res = response.data
-                    mainData.pictureList = res.data
-                    alertify.notify("照片列表获取成功", 'success', 3, function () {
-                    });
-                })
-                .catch(function (error) {
-                    // 处理错误情况
-                    alertify.notify("照片列表获取失败" + error, 'success', 3, function () {
                     });
                     // console.log(error);
                 })
@@ -64,27 +43,6 @@ new Vue({
                     // 处理成功情况
                     // console.log(response);
                     that.$options.methods.hello2()
-                    alertify.notify("相册删除完成", 'success', 3, function () {
-                    });
-                })
-                .catch(function (error) {
-                    // 处理错误情况
-                    alertify.notify("相册删除失败" + error, 'success', 3, function () {
-                    });
-                    // console.log(error);
-                })
-                .then(function () {
-                    // 总是会执行
-                });
-        },
-        // 删除图片信息
-        deletePicture: function (photoId) {
-            const that = this
-            axios.get('/picture/delete?photoId=' + photoId)
-                .then(function (response) {
-                    // 处理成功情况
-                    // console.log(response);
-                    that.$options.methods.hello3()
                     alertify.notify("相册删除完成", 'success', 3, function () {
                     });
                 })
